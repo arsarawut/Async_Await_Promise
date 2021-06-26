f2def4e258c559fc85d9bb38d1055f02a7fa93cf
arsarawut <38756360+arsarawut@users.noreply.github.com>
2021-06-27 04:37:32 +0700

    add Promise.all

--- src/index.js
+++ src/index.js
@@ -15,3 +15,20 @@ const loadData = async () => {
 };
 
 loadData();
+
+//Promise All
+const loadData2 = async () => {
+  try {
+    const url1 = "https://jsonplaceholder.typicode.com/todos/1";
+    const url2 = "https://jsonplaceholder.typicode.com/todos/2";
+    const url3 = "https://jsonplaceholder.typicode.com/todos/3";
+    const results = await Promise.all([fetch(url1), fetch(url2), fetch(url3)]);
+    const dataPromises = await results.map((result) => result.json());
+    const finalData = Promise.all(dataPromises);
+    return finalData;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const data = loadData2().then((data) => console.log(data));
